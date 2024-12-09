e1d62aff2b0820a440a95d1daab28e96f0536db9
JimYang2023 <y20041112@gmail.com>
2024-12-09 16:29:36 +0800

    update Ball.v

--- sources/Ball.v
+++ sources/Ball.v
@@ -44,6 +44,8 @@ reg [31:0] smash_cnt;
 wire check_cnt_max;
 wire check_smash_cnt_max;
 wire palyer_collison;
+wire player_left_side , player_right_side , player_top;
+wire NPC_left_side , NPC_right_side , NPC_top;
 wire NPC_collison;
 wire net_collison;
 wire net_collison_top;
@@ -56,11 +58,17 @@ reg x_dir , y_dir;
 
 
 //assign
-assign player_collison = ((pos_x[31:20] + Ball_W >= Player_X + 5 ) && ( pos_x[31:20] <= Player_X + Pika_W - 7)&&
-                         (pos_y[31:20] + Ball_H >= Player_Y) && ( pos_y[31:20] <= Player_Y + Pika_H));
-
-assign NPC_collison = ((pos_x[31:20] + Ball_W >= NPC_X +5) && ( pos_x[31:20] <= NPC_X + Pika_W -7)&&
-                      ( pos_y[31:20] + Ball_H  >= NPC_Y) && ( pos_y[31:20] <= NPC_Y + Pika_H));
+assign player_left_side = (pos_x[31:20] + Ball_W >= Player_X + 5 );
+assign player_right_side = ( pos_x[31:20] <= Player_X + Pika_W - 7);
+assign player_top = (pos_y[31:20] + Ball_H >= Player_Y);
+assign player_collison = (player_right_side && player_left_side &&(player_top) 
+                         && ( pos_y[31:20] <= Player_Y + Pika_H));
+                                                
+assign NPC_left_side = (pos_x[31:20] + Ball_W >= NPC_X +5);
+assign NPC_right_side = ( pos_x[31:20] <= NPC_X + Pika_W -7);
+assign NPC_top_side = (pos_y[31:20] + Ball_H  >= NPC_Y);
+assign NPC_collison = (NPC_left_side && (NPC_right_side) && (NPC_top_side) && 
+                      ( pos_y[31:20] <= NPC_Y + Pika_H));
 
 assign net_collison = (pos_y[31:20] + Ball_H >= NET_POS_Y) && (pos_x[31:20] + Ball_W >= NET_POS_X) && ( pos_x[31:20] <= NET_POS_X + NET_W);
 assign net_collison_top  =(pos_y[31:20] + Ball_H == NET_POS_Y) && (pos_x[31:20] + Ball_W >= NET_POS_X) && (pos_x[31:20] <= NET_POS_X + NET_W);
@@ -104,16 +112,16 @@ always @(posedge clk)begin
         else if(pos_x[31:20] + Ball_W == VBUF_W) x_dir <= 0;
         else if(net_collison && pos_x[31:20] + Ball_W == NET_POS_X) x_dir <= 0;
         else if(net_collison && pos_x[31:20] == NET_POS_X + NET_W ) x_dir <= 1;
-        else if(player_collison && pos_x[31:20] + Ball_W == Player_X)begin 
+        else if(player_collison && pos_x[31:20] + Ball_W == Player_X + 5 )begin 
             x_dir <= 0;
             v_x <= 2; 
-        end else if(player_collison && pos_x[31:20] == Player_X + Pika_W)begin 
+        end else if(player_collison && pos_x[31:20] == Player_X + Pika_W - 7)begin 
             x_dir <= 1;
             v_x <= 2; 
-        end else if(NPC_collison && pos_x[31:20] + Ball_W == NPC_X)begin 
+        end else if(NPC_collison && pos_x[31:20] + Ball_W == NPC_X + 5 )begin 
             x_dir <= 0;
             v_x <= 2;
-        end else if(NPC_collison && pos_x[31:20] == NPC_X + Pika_W)begin 
+        end else if(NPC_collison && pos_x[31:20] == NPC_X + Pika_W - 7)begin 
             x_dir <= 1;
             v_x <= 2;
         end else x_dir <= x_dir;
@@ -130,7 +138,7 @@ always @(posedge clk)begin
         pos_y <= (y_dir) ? (pos_y + v_y[31:23] * smash_times) : (pos_y - v_y[31:23] * smash_times);
         if(pos_y[31:20] == 0 || v_y[31:23] == 0)y_dir <= 1;
         else if(pos_y[31:20] + Ball_H == VBUF_H - 20) y_dir <= 0;
-        else if( (player_collison && pos_x + Ball_H <= Player_Y + 21) || (NPC_collison && pos_x + Ball_H <= NPC_Y + 21) || net_collison_top) y_dir <= 0;
+        else if( (player_collison) || (NPC_collison) || net_collison_top) y_dir <= 0;
         else y_dir <= y_dir; 
     end
 end
@@ -143,7 +151,7 @@ always @(posedge clk)begin
         v_y[31:23] <= START_V_Y;
     end else begin
         if(player_collison)begin
-            v_y <= 5;
+            v_y[31:23] <= 12'd4;
         end else begin
             v_y <= ( y_dir )? (v_y + g) : ( (v_y < g)?0:(v_y - g) );
         end

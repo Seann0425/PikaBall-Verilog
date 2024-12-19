d2cd586b0bb91244aba02cbed1e5a754059cbea7
Seann <seanyu0425@gmail.com>
2024-12-19 19:52:43 +0800

    npc game mode added

--- sources/npc.v
+++ sources/npc.v
@@ -1,37 +1,19 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/12/10 00:00:04
-// Design Name: 
-// Module Name: npc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module npc(
     input clk,
     input reset_n,
     input  [11:0] ball_pos_x,
     input  [11:0] ball_pos_y,
-    input [1:0] Game_state,//
+    input game_mode, // 0: hard, 1: easy
+    input [1:0] game_state,
     output [11:0] npc_pos_x,
     output [11:0] npc_pos_y
 );
 
 reg [31:0] npc_clock;
 reg [31:0] npc_vclock;
+reg [20:0] doziness_clock; // for easy mode
 
 wire [9:0] pos;
 wire [9:0] pos_v;
@@ -56,16 +38,18 @@ reg face_v;
 
 // npc horizontal movement
 always @(posedge clk) begin
-    if (~reset_n || Game_state == 1) npc_clock[31:20] <= 1;
-    else if (~(npc_clock[31:20] + NPC_W > 160) && ball_pos_x > npc_pos_x + BALL_DIST + npc_clock[1:0])
+    if (~reset_n || game_state == 1) npc_clock[31:20] <= 1;
+    else if (~(npc_clock[31:20] + NPC_W > 160) && ball_pos_x > npc_pos_x + BALL_DIST + npc_clock[1:0]
+            && (game_mode == 0 || doziness_clock < 21'd500_000))
         npc_clock <= npc_clock + 1;
-    else if (~(npc_clock[31:20] <= 0) && ball_pos_x < npc_pos_x + BALL_DIST + npc_clock[1:0])
+    else if (~(npc_clock[31:20] <= 0) && ball_pos_x < npc_pos_x + BALL_DIST + npc_clock[1:0]
+            && (game_mode == 0 || doziness_clock < 21'd500_000))
         npc_clock <= npc_clock - 1;
 end
 
 // npc vertical movement
 always @(posedge clk) begin
-    if (~reset_n || Game_state == 1 ) npc_vclock[31:22] <= VBUF_H - NPC_H - 21;
+    if (~reset_n || game_state == 1 ) npc_vclock[31:22] <= VBUF_H - NPC_H - 21;
     else if (face_v && npc_vclock[31:22] > 0)
         npc_vclock <= npc_vclock - speed;
     else if (~face_v && npc_vclock[31:22] + NPC_H < VBUF_H - 20)
@@ -89,6 +73,13 @@ always @(posedge clk) begin
         speed <= speed + 2;
         speed_clk <= 0;
     end else speed_clk <= speed_clk + 1;
+end
+
+// doziness clock control
+always @(posedge clk) begin
+    if (~reset_n) doziness_clock <= 0;
+    else if (game_mode == 1 && doziness_clock < 21'd1_000_000) doziness_clock <= doziness_clock + 1;
+    else doziness_clock <= 0;
 end
 
 endmodule
